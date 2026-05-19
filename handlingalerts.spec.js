d2adf4dc23e08c57a65b5a077083145c87c3fff9
raghular-collab <raghul.chitra@gmail.com>
2026-05-19 09:38:58 +0530

    Add files via upload

--- handlingalerts.spec.js
+++ handlingalerts.spec.js
@@ -4,10 +4,42 @@ test('simple alerts',async({page})=>{
     await page.goto('https://demoqa.com/alerts');
     await page.locator('#alertButton').click();
     page.on('dialog',async(dialog)=>{
-        if(dialog.type=='alert'){
+        if(dialog.type()=='alert'){
             dialog.accept();
+            console.log('simple alert handled');
         }
-
     })
-
 });
+
+test('confirm alert',async ({page})=>{
+    await page.goto('https://demoqa.com/alerts');
+    await page.locator('#confirmButton').click();
+    page.on('dialog',async(dialog)=>{
+        if(dialog.type()=='confirm'){
+            dialog.accept();
+            console.log('confirm alert handled');
+        }
+    })
+});
+
+test('prompt alert',async({page})=>{
+    await page.goto('https://demoqa.com/alerts');
+    await page.locator('#promtButton').click();
+    page.on('dialog',async(dialog)=>{
+        if(dialog.type()=='prompt'){
+            dialog.accept('accepted');
+            console.log('prompt alert handled');
+        }
+    })
+});
+
+test.only('wait alert',async({page})=>{
+    await page.goto('https://demoqa.com/alerts');
+    await page.locator('#timerAlertButton').click();
+    page.on('dialog',async(dialog)=>{
+        if(dialog.type()=='alert'){
+            dialog.accept();
+            console.log('timer alert handled');
+        }
+    })
+})
