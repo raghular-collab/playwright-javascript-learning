840bac891c4dcc400d895037ae71931e7673a991
raghul-ar <raghul.chitra@gmail.com>
2026-05-22 13:30:49 +0530

    Add files via upload

--- filehandling.spec.js
+++ filehandling.spec.js
@@ -6,8 +6,15 @@ test.only('file handling',async({page})=>{
     await page.locator('#uploadFile').setInputFiles('/Users/raghular/fileupload.html');
     await page.waitForTimeout(2000);
     //to delete uploaded file
-    await page.locator('#uploadFile',[]);
-    await page.waitForTimeout(2000);
+    //await page.locator('#uploadFile').setInputFiles([]);
+    //await page.waitForTimeout(2000);
+    //download a file
+    
+    const [download] = await Promise.all([
+        page.waitForEvent('download'),
+        page.locator('#downloadButton').click()
+    ]);
+    await download.saveAs('/Users/raghular/file.txt');
 });
 
 
@@ -20,7 +27,7 @@ test('multiple file handling',async({page})=>{
     ]);
     await page.waitForTimeout(2000);
     //to delete a specific file from multiple uploads
-    await page.locator('input[type="file"]',['/Users/raghular/fileupload1.html','/Users/raghular/fileupload.html']);
+    await page.locator('input[type="file"]').setInputFiles(['/Users/raghular/fileupload1.html','/Users/raghular/fileupload.html']);
     await page.waitForTimeout(2000);
     await page.locator('input[type="file"]').setInputFiles('/Users/raghular/fileupload2.html');
     await page.waitForTimeout(5000);
